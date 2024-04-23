e51ff745871abb2bb4cc59fc37077deebb681179
backli1ght <1124887276@qq.com>
2024-04-23 17:55:58 +0800

    Register and login function finished.

---
+++ src/main/java/com/nowcoder/community/util/CommunityConstant.java
@@ -0,0 +1,16 @@
+package com.nowcoder.community.util;
+
+public interface CommunityConstant {
+    /**
+     * Activation Success
+     */
+    int ACTIVATION_SUCCESS = 0;
+
+
+    // repeat
+    int ACTIVATION_REPEAT = 1;
+
+    // fail
+
+    int ACTIVATION_FAILURE = 2;
+}
