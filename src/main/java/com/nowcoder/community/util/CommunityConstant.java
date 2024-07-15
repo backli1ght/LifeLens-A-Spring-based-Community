a089b9f8b5b651ce4ac3e53494879c58cda313e4
backli1ght <1124887276@qq.com>
2024-07-15 17:39:53 +0800

    Follow function updated.

--- src/main/java/com/nowcoder/community/util/CommunityConstant.java
+++ src/main/java/com/nowcoder/community/util/CommunityConstant.java
@@ -1,30 +1,45 @@
 package com.nowcoder.community.util;
 
 public interface CommunityConstant {
+
     /**
-     * Activation Success
+     * 激活成功
      */
     int ACTIVATION_SUCCESS = 0;
 
-    // repeat
+    /**
+     * 重复激活
+     */
     int ACTIVATION_REPEAT = 1;
 
-    // fail
+    /**
+     * 激活失败
+     */
     int ACTIVATION_FAILURE = 2;
 
-    // default expiration time
+    /**
+     * 默认状态的登录凭证的超时时间
+     */
     int DEFAULT_EXPIRED_SECONDS = 3600 * 12;
 
-    // remember me expiration time
+    /**
+     * 记住状态的登录凭证超时时间
+     */
     int REMEMBER_EXPIRED_SECONDS = 3600 * 24 * 100;
 
     /**
-     * entityType: discussPost
+     * 实体类型: 帖子
      */
     int ENTITY_TYPE_POST = 1;
 
     /**
-     * entityType: comment
+     * 实体类型: 评论
      */
     int ENTITY_TYPE_COMMENT = 2;
+
+    /**
+     * 实体类型: 用户
+     */
+    int ENTITY_TYPE_USER = 3;
+
 }
--- src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
+++ src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
@@ -5,7 +5,8 @@ public class RedisKeyUtil {
     private static final String SPLIT = ":";
     private static final String PREFIX_ENTITY_LIKE = "like:entity";
     private static final String PREFIX_USER_LIKE = "like:user";
-
+    private static final String PREFIX_FOLLOWEE = "followee";
+    private static final String PREFIX_FOLLOWER = "follower";
     // 某个实体的赞
     // like:entity:entityType:entityId -> set(userId)
     public static String getEntityLikeKey(int entityType, int entityId) {
@@ -16,5 +17,16 @@ public class RedisKeyUtil {
     // like:user:userId -> int
     public static String getUserLikeKey(int userId) {
         return PREFIX_USER_LIKE + SPLIT + userId;
+    }
+
+
+    // followee:userId:entityType -> zset(entityId,now)
+    public static String getFolloweeKey(int userId, int entityType) {
+        return PREFIX_FOLLOWEE + SPLIT + userId + SPLIT + entityType;
+    }
+
+    // follower:entityType:entityId -> zset(userId,now)
+    public static String getFollowerKey(int entityType, int entityId) {
+        return PREFIX_FOLLOWER + SPLIT + entityType + SPLIT + entityId;
     }
 }
