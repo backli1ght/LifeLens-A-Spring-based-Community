5b43b01703294fdb29904b130c76a553243a1c39
backli1ght <1124887276@qq.com>
2024-07-19 15:51:48 +0800

    User Permission Control updated.

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -5,32 +5,35 @@ $(function(){
 function publish() {
 	$("#publishModal").modal("hide");
 
-	// Get the title and content of the post
+	// // 发送AJAX请求之前,将CSRF令牌设置到请求的消息头中.
+   // var token = $("meta[name='_csrf']").attr("content");
+   // var header = $("meta[name='_csrf_header']").attr("content");
+   // $(document).ajaxSend(function(e, xhr, options){
+   //     xhr.setRequestHeader(header, token);
+   // });
+
+	// 获取标题和内容
 	var title = $("#recipient-name").val();
 	var content = $("#message-text").val();
-	// Send the post request
+	// 发送异步请求(POST)
 	$.post(
 		CONTEXT_PATH + "/discuss/add",
-		{"title":title, "content":content},
-		function(data){
+		{"title":title,"content":content},
+		function(data) {
 			data = $.parseJSON(data);
-			// Show the hint message
+			// 在提示框中显示返回消息
 			$("#hintBody").text(data.msg);
-			// Show the hint modal
+			// 显示提示框
 			$("#hintModal").modal("show");
-			// Hide the hint modal after 2 seconds
+			// 2秒后,自动隐藏提示框
 			setTimeout(function(){
 				$("#hintModal").modal("hide");
-				// Refresh the page
-				if(data.code == 0){
+				// 刷新页面
+				if(data.code == 0) {
 					window.location.reload();
 				}
 			}, 2000);
 		}
-	)
+	);
 
-	$("#hintModal").modal("show");
-	setTimeout(function(){
-		$("#hintModal").modal("hide");
-	}, 2000);
 }
